ef9368a6aee1d2b1a2f19034ad604062e33442ac
oorspronklikheid <oorspronklikheid@gmail.com>
2022-10-25 06:05:52 +0200

    Syncy  , now with 100% more ota

--- basicweb.ino
+++ basicweb.ino
@@ -1,39 +1,9 @@
- 
- /*
- WiFi Web Server LED Blink
-
- A simple web server that lets you blink an LED via the web.
- This sketch will print the IP address of your WiFi Shield (once connected)
- to the Serial monitor. From there, you can open that address in a web browser
- to turn on and off the LED on pin 5.
-
- If the IP address of your shield is yourAddress:
- http://yourAddress/H turns the LED on
- http://yourAddress/L turns it off
-
- This example is written for a network using WPA encryption. For
- WEP or WPA, change the Wifi.begin() call accordingly.
-
- Circuit:
- * WiFi shield attached
- * LED attached to pin 5
-
- created for arduino 25 Nov 2012
- by Tom Igoe
-
-ported for sparkfun esp32 \\
-31.01.2017 by Jan Hendrik Berlin
- 
- */
 #include <EEPROM.h>
 #include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 // use first channel of 16 channels (started from zero)
-/*
-#define LEDC_CHANNEL_0     5
-#define LEDC_CHANNEL_1     17
-#define LEDC_CHANNEL_2     16
-#define LEDC_CHANNEL_3     4
-//*/
 //*
 #define LEDC_CHANNEL_0     0
 #define LEDC_CHANNEL_1     1
@@ -44,7 +14,7 @@ ported for sparkfun esp32 \\
 #define LEDC_TIMER_13_BIT  13
 
 // use 5000 Hz as a LEDC base frequency
-#define LEDC_BASE_FREQ     50000
+#define LEDC_BASE_FREQ     1000000
 
 // fade LED PIN (replace with LED_BUILTIN constant for built-in LED)
 #define LED_PIN1            5
@@ -90,12 +60,11 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
 
 void setup()
 {
-  WiFi.mode(WIFI_STA);
-  // Setup timer and attach timer to a led pin
   
-    Serial.begin(115200);
-    //pinMode(5, OUTPUT);      // set the LED pin mode
-    
+   Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
   ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
   ledcSetup(LEDC_CHANNEL_1, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
   ledcSetup(LEDC_CHANNEL_2, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
@@ -106,23 +75,56 @@ void setup()
   ledcAttachPin(PWM3, LEDC_CHANNEL_2);
   ledcAttachPin(PWM4, LEDC_CHANNEL_3);
 
-
-    delay(10);
    // Configures static IP address
     if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS)) {
       Serial.println("STA Failed to configure");
     }
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  
+  ArduinoOTA.setPort(24663);
+  
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
     // We start by connecting to a WiFi network
 
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(ssid);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
+//    WiFi.mode(WIFI_STA);
+//    WiFi.begin(ssid, password);
+//    while (WiFi.status() != WL_CONNECTED) {
+//        delay(500);
+//        Serial.print(".");
+//    }
 
     Serial.println("");
     Serial.println("WiFi connected.");
@@ -155,19 +157,13 @@ int value = 0;
 int disconnectedtimer = 0 ; 
 int statscounter = 0 ; 
 void loop(){
+  
+  ArduinoOTA.handle();
+
     statscounter++; 
     if(statscounter==100000)
     {
       statscounter= 0 ; 
-      /*
-      Serial.println(WiFi.status());
-      Serial.println("32");
-      Serial.println(digitalRead(32));
-      Serial.println("35");
-      Serial.println(digitalRead(35));
-      Serial.println("34");
-      Serial.println(digitalRead(34));
-      */
     }
     //check if we still are connected to ap
 //    delay(500);
